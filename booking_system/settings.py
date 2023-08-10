e4f18d4a7c8235b783ab8a459afe86e3adf2e4d7
rodriguezroberto <iamrobertorodriguez@proton.me>
2023-08-10 00:57:19 -0600

    removing heroku

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -1,5 +1,4 @@
 import environ
-import django_heroku
 from pathlib import Path
 from datetime import timedelta
 
@@ -155,7 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-
-# Setting static files for deploying
-django_heroku.settings(locals())
